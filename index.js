9e8f37ada12c3faffe07a22842c9cdcf4f6dd8f0
ahnafabir <ahnafabir313@gmail.com>
2021-04-13 08:12:54 +0600

    till mongodb image upload

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 require('dotenv').config()
 const cors = require('cors')
+const fs = require('fs-extra')
 const express = require('express')
+const fileUpload = require('express-fileupload')
 
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors())
+app.use(fileUpload())
 app.use(express.json())
+app.use(express.static('doctors'))
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
@@ -19,6 +23,7 @@ const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const appointmentCollection = client.db(`${process.env.DB_NAME}`).collection("appointments");
+  const doctorsCollection = client.db(`${process.env.DB_NAME}`).collection("doctors");
 
   app.post('/addAppointment', (req, res) => {
     const appointmentData = req.body;
@@ -30,11 +35,23 @@ client.connect(err => {
 
   app.post('/appointmentsByDate', (req, res) => {
     const date = req.body.date;
-       
-    appointmentCollection.find({date})
-    .toArray((err, appointments)=> {
-      res.send(appointments)
-    })
+    const email = req.body.email;
+
+    doctorsCollection.find({email: email})
+      .toArray((err, doctors)=> {
+        if(doctors.length === 0) {
+          appointmentCollection.find({ date, email })
+            .toArray((err, appointments) => {
+              res.send(appointments)
+            })
+        } else {
+          appointmentCollection.find({ date })
+            .toArray((err, appointments) => {
+              res.send(appointments)
+            })
+        }
+      })
+    
   })
 
   app.get('/appointments', (req, res) => {
@@ -44,7 +61,37 @@ client.connect(err => {
     })
   })
 
+  app.post('/addADoctor', (req, res) => {
+    const {name, email} =  req.body;
+    const file = req.files.file;
+    const filePath = `${__dirname}/doctors/${file.name}`;
+    
+    file.mv(filePath, (err)=> {
+      res.status(500).send({msg: 'Failed To Upload Image'})
+    })
+
+    const newImg = fs.readFileSync(filePath);
+    const encImg = newImg.toString('base64');
+    const image = {
+      contentType: file.mimetype,
+      size: file.size,
+      img: Buffer.from(encImg, 'base64')
+    }
+
+    doctorsCollection.insertOne({name, email, image})
+      .then(result => {
+        fs.remove(filePath)
+      })
+  })
+
+  app.get('/doctors', (req, res) => {
+    doctorsCollection.find()
+    .toArray((err, doctors)=> {
+      res.send(doctors)
+    })
+  })
+
 });
 
 
-app.listen(port, () => console.log(`Server Running at http://localhost:${port}`))
+app.listen(port)
